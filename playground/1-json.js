e8bcb84c73ff11d3962c793bfe461a0d45aa5aec
rajan <rajangreedge@gmail.com>
2019-03-03 05:12:43 +0530

    challenge

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,30 @@
-const book = {
+const fs =require("fs");
+/* const book = {
     title: 'Eggo is the enemy',
     author: 'Ryan Holiday'
 }
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+
+const parseData = JSON.parse(bookJSON);
+
+fs.writeFileSync('1-json.json', bookJSON);
+const dataBuffer = fs.readFileSync('1-json.json');
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data); */
+
+/**Challenge */
+
+//1
+const dataBuffer = fs.readFileSync("1-json.json");
+const dataString = dataBuffer.toString();
+const data = JSON.parse(dataString);
+//2
+data.name = "Arjun";
+data.age = 26;
+//3
+const newDataStr = JSON.stringify(data);
+fs.writeFileSync("1-json.json",newDataStr);
+
+
