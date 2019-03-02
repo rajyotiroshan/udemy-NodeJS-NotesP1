789d55b0f096875ad9f72732e1c4e66c1353cd68
rajan <rajangreedge@gmail.com>
2019-03-03 04:51:21 +0530

    added builder for add cmd

--- app.js
+++ app.js
@@ -12,8 +12,21 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function() {
-        console.log('Adding a new note');
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe:'',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title::', argv.title);
+        console.log('Body:: ', argv.body);
     }
 });
 
@@ -49,37 +62,5 @@ yargs.command({
 });
 
 //
-console.log(yargs.argv);
+yargs.parse();
 
-
-
-/* const argv = yargs.argv;
-var cmd = argv._[0];//proce */
- 
-/*  if(cmd === 'add'){//add
-    console.log("Adding new node.");
-    let note = notes.addNote(argv.title,argv.body);
-    if(note) {
-        console.log("inserted notes::" + note);
-    }else {
-        console.log("already inserted");
-    }
-}else if(cmd === 'list') {//list
-    console.log('Listing all nodes');
-    notes.getAll();
-}
-else if(cmd === 'read'){//read
-    let receivedNote = notes.getNote(argv.title);
-    if(receivedNote.length > 0) {
-        let note = {title:receivedNote[0].title, body:receivedNote[0].body};
-        console.log("Received Note::" + note);
-    }else {
-        console.log("Note with title::" + argv.title + " does not exist.");
-    }
-}else if(cmd === 'remove') {//remove
-    let noteRemoved = notes.removeNote(argv.title);
-    let msg = noteRemoved?"note removed":"note does not exist";
-    console.log(msg);
-}else {
-    console.log("Command::" + cmd +' is Not Recognized')
-}  */
