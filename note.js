65221d3d1fa843795c670bd7f4f68da796b2f87a
rajan <rajangreedge@gmail.com>
2019-03-03 06:57:26 +0530

    refactor addNote to use array find method

--- note.js
+++ note.js
@@ -7,9 +7,9 @@ const getNotes = function() {
 /**Add new note */
 const addNote = (title, body) =>{
     const notes =  loadNotes();
-    const duplicateNotes = notes.filter((note)=>note.title === title);
-    
-    if(duplicateNotes.length === 0 ){
+    //const duplicateNotes = notes.filter((note)=>note.title === title);
+    const duplicateNote = notes.find((note)=>note.title===title);
+    if(!duplicateNote ){
         notes.push({
             title: title,
             body: body
